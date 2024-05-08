5d4178f96df54ed9137682d53b9dfab071e766e8
souorichard <elaine.ra96@gmail.com>
2024-05-07 21:12:42 -0300

    feat: add login page

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -2,9 +2,11 @@ import { PropsWithChildren } from 'react'
 
 export default function AuthLayout({ children }: PropsWithChildren) {
   return (
-    <div>
-      {children}
-      <section>Banner</section>
+    <div className="h-screen p-20 flex justify-center items-center bg-zinc-200">
+      <div className="w-full h-full grid grid-cols-2 bg-background rounded-3xl shadow-2xl">
+        {children}
+        <section className="bg-auth bg-cover rounded-e-3xl" />
+      </div>
     </div>
   )
 }
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,7 +1,38 @@
+import { Button } from '@/components/ui/button'
+import { LoginForm } from './_components/login-form'
+import { Separator } from '@/components/ui/separator'
+import Link from 'next/link'
+
 export default function LoginPage() {
   return (
-    <section>
-      <h1>Login Page</h1>
+    <section className="p-10 flex justify-center items-center">
+      <div className="max-w-sm w-full flex flex-col items-center space-y-8">
+        <div className="space-y-2 text-center">
+          <h1 className="text-3xl font-bold">Faça Login na Buscavan</h1>
+          <p className="text-muted-foreground">
+            Preencha os dados abaixo para entrar no sistema
+          </p>
+        </div>
+
+        <LoginForm />
+
+        <div className="w-full flex items-center space-x-3">
+          <Separator className="flex-1" />
+
+          <p className="text-xs text-muted-foreground">OU</p>
+
+          <Separator className="flex-1" />
+        </div>
+
+        <Button variant="outline" className="w-full" asChild>
+          <Link href="/auth/register">Cadastre-se em nossa plataforma</Link>
+        </Button>
+
+        <p className="text-sm text-muted-foreground text-center">
+          Ao continuar, voce concorda com os Termos de Serviço e Políticas de
+          Privacidade
+        </p>
+      </div>
     </section>
   )
 }
