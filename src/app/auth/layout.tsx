c4b45392df7ff9953b0a78ebe424914006c7e560
Lucas <lucasvieirasilva.lpta@gmail.com>
2024-05-10 21:47:46 -0300

    fix: container sizing

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from 'react'
 export default function AuthLayout({ children }: PropsWithChildren) {
   return (
     <div className="h-screen p-20 flex justify-center items-center bg-zinc-200">
-      <div className="max-w-7xl w-full max-h-fit h-full grid grid-cols-2 bg-background rounded-lg shadow-2xl">
+      <div className="max-w-7xl w-full max-h-fit h-max grid grid-cols-2 bg-background rounded-lg shadow-2xl">
         <section className="bg-auth bg-cover rounded-s-lg" />
         {children}
       </div>
