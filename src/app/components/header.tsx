5fb6c683faaa3aaa651d79b33369b33be6d6571f
Lucas <lucasvieirasilva.lpta@gmail.com>
2024-05-12 17:36:19 -0300

    refactor: add link tag to buttons

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,13 +1,18 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export function Header() {
   return (
     <header className="w-full h-18 px-32 py-6 flex items-center justify-between">
       <Image src={'buscavan.svg'} width={140} height={20} alt="Buscavan Logo" />
       <div className="w-fit h-full flex items-center justify-end gap-2">
-        <Button variant={'ghost'}>Entrar</Button>
-        <Button>Cadastrar-se</Button>
+        <Link href={'auth/login'}>
+          <Button variant={'ghost'}>Entrar</Button>
+        </Link>
+        <Link href={'auth/register'}>
+          <Button>Cadastrar-se</Button>
+        </Link>
       </div>
     </header>
   )
