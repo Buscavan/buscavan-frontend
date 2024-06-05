8f0db6aa76924add79371743407f3832f55d5ee7
LucasVieiraS <lucasvieirasilva.lpta@gmail.com>
2024-06-04 23:36:00 -0300

    feat: redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/inicio',
+        permanent: true,
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
