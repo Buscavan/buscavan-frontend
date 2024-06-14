1caff5bb685f969158d89007ec1b5e9844ae60b7
souorichard <elaine.ra96@gmail.com>
2024-06-14 14:03:45 -0300

    change: new structure of header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -11,31 +11,33 @@ export function Header() {
   const { user } = useAuth()
 
   return (
-    <header className="w-full h-18 px-32 py-6 flex items-center justify-between border border-gray-300">
-      <div className="w-fit h-full flex items-center gap-2">
-        {!user && <SideDrawer />}
-        <Link href={'/'}>
-          <Image
-            src={'buscavan.svg'}
-            width={140}
-            height={20}
-            alt="Buscavan Logo"
-          />
-        </Link>
-      </div>
-      <div className="w-fit h-full flex items-center justify-end gap-2">
-        {!user ? (
-          <Dropdown />
-        ) : (
-          <>
-            <Link href={'auth/login'}>
-              <Button variant={'ghost'}>Entrar</Button>
-            </Link>
-            <Link href={'auth/register'}>
-              <Button>Cadastrar-se</Button>
-            </Link>
-          </>
-        )}
+    <header className="fixed top-0 left-0 w-full h-16 flex items-center bg-background border border-gray-300">
+      <div className="max-w-7xl w-full mx-auto flex justify-between items-center">
+        <div className="w-fit h-full flex items-center gap-2">
+          {!user && <SideDrawer />}
+          <Link href={'/'}>
+            <Image
+              src={'buscavan.svg'}
+              width={140}
+              height={20}
+              alt="Buscavan Logo"
+            />
+          </Link>
+        </div>
+        <div className="w-fit h-full flex items-center justify-end gap-2">
+          {!user ? (
+            <Dropdown />
+          ) : (
+            <>
+              <Link href={'auth/login'}>
+                <Button variant={'ghost'}>Entrar</Button>
+              </Link>
+              <Link href={'auth/register'}>
+                <Button>Cadastrar-se</Button>
+              </Link>
+            </>
+          )}
+        </div>
       </div>
     </header>
   )
