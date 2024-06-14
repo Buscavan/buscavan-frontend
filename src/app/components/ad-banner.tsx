c614b8584282b6cbb801411603b732520b854e44
souorichard <elaine.ra96@gmail.com>
2024-06-14 14:42:15 -0300

    change: new structure of ad-banner

--- src/app/components/ad-banner.tsx
+++ src/app/components/ad-banner.tsx
@@ -29,19 +29,17 @@ export function AdBanner() {
   return (
     <div
       style={{ opacity }}
-      className="w-full h-20 bg-zinc-800 px-32 py-4 flex justify-between items-center fixed bottom-0 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]"
+      className="w-full h-14 bg-zinc-800 flex items-center fixed bottom-0 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]"
     >
-      <p className="flex-1 text-white font-medium text-lg">
-        Anuncie em nossa plataforma e entre pro time da Buscavan!
-      </p>
-      <div className="flex-1 flex justify-end items-center h-full">
-        <Button
-          className="h-full"
-          variant={'secondary'}
-          disabled={opacity <= 0.5}
-        >
-          Cadastrar Veículos
-        </Button>
+      <div className="max-w-7xl w-full mx-auto flex items-center gap-3">
+        <p className="flex-1 text-white font-medium">
+          Anuncie em nossa plataforma e entre pro time da Buscavan!
+        </p>
+        <div className="flex-1 flex justify-end items-center h-full">
+          <Button size="sm" variant={'secondary'} disabled={opacity <= 0.5}>
+            Cadastrar Veículos
+          </Button>
+        </div>
       </div>
     </div>
   )
