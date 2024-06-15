ff1ec8c9b0b8c8cf985cddcf92fce09a5a5355f3
souorichard <elaine.ra96@gmail.com>
2024-06-15 19:46:52 -0300

    feat: add application-page component (composition patterns)

---
+++ src/components/application/page.tsx
@@ -0,0 +1,44 @@
+import { cn } from '@/lib/utils'
+import { ReactNode } from 'react'
+
+type ApplicationPageGenericProps<T = unknown> = {
+  className?: string
+  children: ReactNode
+} & T
+export function ApplicationPage({
+  className,
+  children,
+}: ApplicationPageGenericProps) {
+  return (
+    <main className={cn('mt-16 p-10 space-y-5', className)}>{children}</main>
+  )
+}
+
+export function ApplicationPageHeader({
+  className,
+  children,
+}: ApplicationPageGenericProps) {
+  return (
+    <header
+      className={cn('max-w-7xl w-full mx-auto flex items-center', className)}
+    >
+      {children}
+    </header>
+  )
+}
+
+export function ApplicationPageHeaderTitle({
+  className,
+  children,
+}: ApplicationPageGenericProps) {
+  return <h1 className={cn('text-3xl font-bold', className)}>{children}</h1>
+}
+
+export function ApplicationPageContent({
+  className,
+  children,
+}: ApplicationPageGenericProps) {
+  return (
+    <main className={cn('flex flex-col gap-3', className)}>{children}</main>
+  )
+}
