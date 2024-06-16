d562242dcff4eac3c93713cc4051a9e7b1af5011
souorichard <elaine.ra96@gmail.com>
2024-06-16 15:31:33 -0300

    feat: add vehicle-card component

--- src/app/app/search/page.tsx
+++ src/app/app/search/page.tsx
@@ -6,6 +6,14 @@ import {
   ApplicationPageHeaderTitle,
 } from '@/components/application/page'
 import { SearchForm } from './_components/search-form'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { VehicleCard } from './_components/vehicle-card'
 
 export const metadata: Metadata = {
   title: 'Viagens',
@@ -22,6 +30,27 @@ export default function Search() {
 
       <ApplicationPageContent>
         <SearchForm />
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Resultados</CardTitle>
+            <CardDescription>
+              Veículos disponíveis para sua viagem.
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent className="grid grid-cols-4 gap-4">
+            {Array.from({ length: 8 }).map((_, i) => (
+              <VehicleCard
+                key={i}
+                vehicleName="Mercedes Sprinter"
+                description="Teste"
+                price={1000}
+                date="16/10/2024"
+              />
+            ))}
+          </CardContent>
+        </Card>
       </ApplicationPageContent>
     </ApplicationPage>
   )
