b88fa06475c9c673bf0259ad5b23c9e6e0452dd3
souorichard <elaine.ra96@gmail.com>
2024-06-17 20:18:24 -0300

    feat: add theme-provider (next-themes)]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { Toaster } from '@/components/ui/toaster'
 import { AuthProvider } from '@/hooks/useAuth'
+import { ThemeProvider } from '@/components/application/theme-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,7 +22,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`custom-scrollbar ${inter.className}`}>
-        <AuthProvider>{children}</AuthProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <AuthProvider>{children}</AuthProvider>
+        </ThemeProvider>
         <Toaster />
       </body>
     </html>
