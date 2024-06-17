cd1d1754bcd11047e8ff3d8eb1cd270825da4c54
LucasVieiraS <lucasvieirasilva.lpta@gmail.com>
2024-06-17 13:28:04 -0300

    feat: auth bg

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,10 +1,18 @@
+'use client'
+
 import { PropsWithChildren } from 'react'
+import { usePathname } from 'next/navigation'
 
 export default function AuthLayout({ children }: PropsWithChildren) {
+  const pathname = usePathname()
+  const isRegisterPage = pathname.includes('/register')
+
   return (
     <div className="h-screen grid grid-cols-1 xl:grid-cols-2">
       {children}
-      <section className="hidden xl:block bg-auth bg-cover" />
+      <section
+        className={`hidden xl:block ${isRegisterPage ? 'bg-register' : 'bg-auth'} bg-cover`}
+      />
     </div>
   )
 }
