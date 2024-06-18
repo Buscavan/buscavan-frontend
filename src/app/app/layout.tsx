270e6d7acd3010a0168175042b6298d5ced51135
souorichard <elaine.ra96@gmail.com>
2024-06-18 12:25:46 -0300

    style: fix background color

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -3,7 +3,7 @@ import { MainHeader } from './_components/main-header'
 
 export default function AppLayout({ children }: PropsWithChildren) {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-muted dark:bg-muted/10">
       <MainHeader />
       {children}
     </div>
