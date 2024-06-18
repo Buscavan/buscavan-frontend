d814ca017029a803fea48cfe236e8911bc6b13cd
LucasVieiraS <lucasvieirasilva.lpta@gmail.com>
2024-06-17 23:38:15 -0300

    refactor: style and content

--- src/app/app/about/page.tsx
+++ src/app/app/about/page.tsx
@@ -8,30 +8,36 @@ export const metadata: Metadata = {
 export default function About() {
   return (
     <>
-      <article className="w-full h-fit px-32 pb-0 py-16">
+      <article className="w-full h-fit px-80 py-16">
         <h1 className="font-bold text-lg pb-4">Quem somos?</h1>
+        <p className="pb-4">
+          Bem-vindo ao <strong>BUSCAVAN</strong>, a plataforma de busca de vans
+          e outros tipos de veículos para viagens em grupo ou turismo. Nosso
+          objetivo é conectar motoristas e proprietários de veículos com
+          viajantes, garantindo a segurança e a confiabilidade de todas as
+          viagens realizadas através da nossa plataforma.
+        </p>
+        <p className="pb-4">
+          No <strong>BUSCAVAN</strong>, qualquer pessoa, seja formal ou
+          informal, pode cadastrar seu veículo, fornecendo todas as informações
+          necessárias para garantir a segurança e evitar fraudes. Por outro
+          lado, os viajantes podem realizar pesquisas detalhadas para encontrar
+          a melhor opção de viagem, com a certeza de que todas as informações do
+          motorista e do veículo são confiáveis.
+        </p>
+        <p className="pb-4">
+          <strong>Plataforma de Conexão</strong>
+          <br />
+          Somos uma equipe apaixonada por conectar pessoas, dedicando-nos a
+          oferecer a melhor experiência para motoristas e viajantes. Nosso
+          objetivo é tornar a reserva de viagens um processo rápido e fácil.
+        </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur. Sit nam porttitor consequat
-          pulvinar tellus pellentesque. Pulvinar vitae faucibus ut non tincidunt
-          tortor nisi. Neque nibh nunc odio sed tellus interdum aliquam sed.
-          Fermentum ante urna enim vitae turpis mauris sodales vitae ultrices.
-          Nisl ullamcorper et tempus quam sit. Risus cras eu diam vel eget nulla
-          eros. Felis malesuada nibh pellentesque est hendrerit augue. Amet
-          vestibulum volutpat viverra turpis odio nibh arcu. Tempus urna cursus
-          mattis habitasse arcu urna sed etiam amet. Ullamcorper nunc a
-          consequat nisl. In a nisi eu faucibus nisl sollicitudin elementum.
-          Lacus urna massa neque nec tellus risus pulvinar. Amet congue mauris
-          posuere amet elit. Risus mi ut arcu consectetur molestie. Suspendisse
-          lacus interdum amet aliquet nulla. Nunc consectetur platea suspendisse
-          dui fermentum aliquam mi ultricies quam. Maecenas tincidunt leo sit
-          vulputate. Pellentesque molestie faucibus faucibus in porttitor
-          lobortis risus. Nunc ipsum ullamcorper egestas sit mauris suscipit
-          interdum justo. Tellus arcu tempus penatibus in aliquam. Adipiscing
-          cras ornare a eget massa in convallis urna. At risus diam placerat
-          enim pretium potenti purus tempor. Urna nunc sit tellus ultrices
-          senectus nunc. Praesent in etiam urna netus sollicitudin pulvinar.
-          Velit magna et urna quis diam scelerisque. Enim elementum arcu ut
-          suspendisse tortor id.
+          Com o <strong>BUSCAVAN</strong>, promovemos tanto o negócio daqueles
+          que possuem um veículo e desejam gerar renda, quanto o lazer dos
+          cidadãos que buscam viagens seguras e agradáveis. Junte-se a nós e
+          descubra como é fácil e seguro viajar em grupo com o{' '}
+          <strong>BUSCAVAN</strong>!
         </p>
       </article>
       <Credits />
